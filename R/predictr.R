9c28d5f44fd95c5aaef06426c7a758bbc65e22dc
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-11-08 21:37:02 -0500

    I created package documentation.

---
+++ R/predictr.R
@@ -0,0 +1,14 @@
+#' predictr: A package for fast model predictions
+#'
+#' The predictr package provides adaptors which convert standard
+#' model objects into a new type of object.
+#' This new object stores the minimal amount of data required to
+#' run model predictions,
+#' and these new predictions reproduce the results of the
+#' \code{predict} method with greater speed and with less memory
+#' usage.
+#'
+#'
+#' @docType package
+#' @name predictr
+NULL
