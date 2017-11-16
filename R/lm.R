8ee5b1ca5bf77318144ff827eed3077619e74d26
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-11-15 20:16:11 -0500

    I added a variable for the lm intercept string.

--- R/lm.R
+++ R/lm.R
@@ -1,13 +1,18 @@
 #' @include intercept.R
+#' @include linear.R
 NULL
+
+lm_intercept_string <- "(Intercept)"
 
 as.intercept_list.lm <- function(x, ...) {
   y <- coef(x)
-  if (!("(Intercept)" %in% names(y)))
+  if (!(lm_intercept_string %in% names(y)))
     return(list())
 
-  n <- which(names(y) == "(Intercept)")
+  n <- which(names(y) == lm_intercept_string)
   n <- y[n]
   names(n) <- NULL
   list(intercept(n))
 }
+
+
