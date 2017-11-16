d92e8754082f29d5540f101b99d5898d7e7d33d1
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-11-15 20:48:40 -0500

    I added a new predictor method.

--- R/predictor.R
+++ R/predictor.R
@@ -2,3 +2,16 @@
 #' @include linear.R
 NULL
 
+#' Returns a predictor object
+#'
+#' \code{predictor} returns a predictor object
+#'
+#' @param x (Model) object
+#'
+#' @param ... Additional options
+#'
+#' @return A predictor object corresponding with \code{x}
+#'
+#' @export
+predictor <- function(x, ...)
+  UseMethod("predictor")
