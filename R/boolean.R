ca0250175381de7f19bdc13fccbbdef7a388761d
Craig Grabowski <craig.a.grabowski@gmail.com>
2017-11-21 19:09:09 -0500

    I added a boolean factory function.

--- R/boolean.R
+++ R/boolean.R
@@ -2,3 +2,19 @@
 #' @include linear.R
 NULL
 
+Boolean <-
+setClass("Boolean", contains = "Vector")
+
+#' Returns a boolean object
+#'
+#' \code{linear} returns an boolean object.
+#'
+#'
+#' @param x Name of object
+#'
+#' @param data vector (defaults to 0)
+#'
+#' @export
+boolean <- function(x, data = 0) {
+  Boolean(name = x, data = as.numeric(data))
+}
